289ec18efb03a6e0bf9f4b3c77b59d764af77bf7
Kerem <63135398+Kerem-git@users.noreply.github.com>
2021-05-27 05:43:14 +0300

    Add files via upload

---
+++ rasp.py
@@ -0,0 +1,42 @@
+import requests
+from bs4 import BeautifulSoup
+import smtplib
+from time import sleep
+
+email = "mahmutcarlsen@gmail.com"
+password = "Kerem2894" 
+mails= ["keremtowork@gmail.com","keremtowork@gmail.com"]
+sleep(3600)
+urlw = "https://market.samm.com/raspberry-pi-zero-w-en"
+urlwh = "https://market.samm.com/raspberry-pi-zero-wh-en"
+versionW = requests.get(urlw)
+versionWh = requests.get(urlwh)
+Soupw = BeautifulSoup(versionW.content,"html5lib")
+Soupwh = BeautifulSoup(versionWh.content,"html5lib")
+w = Soupw.find("div",{"class":"fl col-12 mb-50 stock-durum2"}).text
+wh = Soupwh.find("div",{"class":"fl col-12 mb-50 stock-durum2"}).text
+print (wh)
+print(mails[0])
+if "Out of stock" not in w:
+    with smtplib.SMTP("smtp.gmail.com",587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.ehlo()
+
+        smtp.login(email, password)
+        subject = "Kerem'e haber verin Pi ZERO W geldi kankii diyee."
+        body = "Kerem'e haber verin Pi ZERO W geldi kankii diyee."
+        msg = f"Subject: {subject}\n\n{body}"
+        smtp.sendmail(email,mails[0],msg)
+
+if "Out of stock" not in wh:
+    with smtplib.SMTP("smtp.gmail.com",587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.ehlo()
+
+        smtp.login(email, password)
+        subject = "Kerem'e haber verin Pi ZERO W geldi kankii diyee."
+        body = "Kerem'e haber verin Pi ZERO W geldi kankii diyee."
+        msg = f"Subject: {subject}\n\n{body}"
+        smtp.sendmail(email,mails[0],msg)
